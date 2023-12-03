55c973e349246f3a509666d148ec4600bbf0c036
youngandgeek <eng.emanessam@gmail.com>
2023-12-03 21:24:09 +0200

    -

--- Controllers/DeptController.cs
+++ Controllers/DeptController.cs
@@ -7,26 +7,17 @@ namespace ModelDbCon.Controllers
 {
     public class DeptController : Controller
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         DbConClass context = new DbConClass();
 
-=======
->>>>>>> 44fc04c9f338df2b74800cb9597a2b7068ade73c
-=======
->>>>>>> e3789e6ace9ab11a19b7d47e80b46bae6b3e7efc
         //index-> getAll Departments method 
         public IActionResult Index()
         {
             //refrence to dbConClass 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
             DbConClass context = new DbConClass();
->>>>>>> 44fc04c9f338df2b74800cb9597a2b7068ade73c
-=======
+
             DbConClass context = new DbConClass();
->>>>>>> e3789e6ace9ab11a19b7d47e80b46bae6b3e7efc
             
             //return the db set values into a list
 
@@ -35,8 +26,6 @@ namespace ModelDbCon.Controllers
             //pass the viewName and  the list to the  
             return View("dept",deptsModel);
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         //return specific department detail, pass the id and check with FirstOrDefault the first match to this Id-> return it
         public IActionResult Detail(int id) {
@@ -74,10 +63,7 @@ namespace ModelDbCon.Controllers
                 //return RedirectToAction("DEatils", new{ id=dept.Id});
             }
         }
-=======
     }
->>>>>>> 44fc04c9f338df2b74800cb9597a2b7068ade73c
-=======
+
     }
->>>>>>> e3789e6ace9ab11a19b7d47e80b46bae6b3e7efc
 }
